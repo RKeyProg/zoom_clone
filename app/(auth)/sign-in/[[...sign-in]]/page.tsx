2d46490a5f10df074abe47f44e581d215cb6c481
Kirill Radkevich <rkey_rs@mail.ru>
2024-04-11 23:40:57 +0300

    change sign-in

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
+export function generateStaticParams() {
+	return [{ id: '1' }, { id: '2' }, { id: '3' }]
+}
+
 const SignInPage = () => {
 	return (
 		<main className='flex-center h-screen w-full'>
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 
+export function generateStaticParams() {
+	return [{ id: '1' }, { id: '2' }, { id: '3' }]
+}
+
 const SignUpPage = () => {
 	return (
 		<main className='flex-center h-screen w-full'>
